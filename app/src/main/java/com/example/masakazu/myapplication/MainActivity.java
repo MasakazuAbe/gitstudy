59a40d847e04e9631e082687746e1eca76ca9ca9
阿部正和 <Masakazu@abemasakazu-no-MacBook-Air.local>
2015-12-16 18:02:41 +0900

    ボタンに反応とか追加

--- app/src/main/java/com/example/masakazu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/masakazu/myapplication/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.masakazu.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    //activity_main.xmlで作ったidを、ここで作ったアイテムに関連付ける
+    @Bind(R.id.from_edit)
+    EditText mFromEdit;  //mFromEditはfrom_editのidがついたEditTextの事ですよという意味
+    @Bind(R.id.from_unit)
+    Button mFromButton;
+    @Bind(R.id.to_text)
+    TextView mToText;
+    @Bind(R.id.to_unit)
+    Button mToButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.from_unit)
+    void onClickFromUnitButton() {
+        Toast.makeText(this, "onClickFromUnitButton", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.to_unit)
+    void onClickToUnitButton() {
+        Toast.makeText(this, "onClickToUnitButton", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.button_calc)
+    void onClickCalc() {
+        Toast.makeText(this, "onClickCalc", Toast.LENGTH_SHORT).show();
     }
 }
