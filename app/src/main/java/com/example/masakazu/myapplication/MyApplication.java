d443462b95c3a9724c20d9796d82dce40294f663
阿部正和 <Masakazu@abemasakazu-no-MacBook-Air.local>
2015-12-18 21:56:48 +0900

    Stetho-RealmでRealmの中身を見れるようにする

--- app/src/main/java/com/example/masakazu/myapplication/MyApplication.java
+++ app/src/main/java/com/example/masakazu/myapplication/MyApplication.java
@@ -3,6 +3,7 @@ package com.example.masakazu.myapplication;
 import android.app.Application;
 
 import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
 
 /**
  * Created by Masakazu on 15/12/18.
@@ -13,8 +14,9 @@ public class MyApplication extends Application {
         super.onCreate();
         Stetho.initialize(
                 Stetho.newInitializerBuilder(this)
-                .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
-                .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
-                .build());
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build());
     }
 }
